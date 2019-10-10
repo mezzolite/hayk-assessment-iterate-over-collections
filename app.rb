f58fad08fb5d7c4f7a17bfc21b16e47110f663a8
mezzolite <mez.charney@gmail.com>
2019-10-10 09:36:52 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Company
     attr_accessor :name, :size
 
@@ -9,8 +11,42 @@ end
 
 companies = [
     Company.new('Alpha', 30),
-    Company.new('Beta, 300),
-    Company.new('Delta, 3000)
+    Company.new('Beta', 300),
+    Company.new('Delta', 3000)
 ]
 
 ### Your code below this
+
+companies.map do |company|
+    puts "#{company.name} - #{company.size}"
+end
+
+company_size = []
+companies.map do |company|
+    company_size << company.size 
+end
+company_size = company_size.reduce(:+)
+puts company_size
+
+big_companies = []
+companies.map do |company|
+    if company.size > 100
+        big_companies << company.name
+    end
+    puts big_companies
+end
+
+beta_company = ""
+companies.map do |company|
+   if company.name == "Beta"
+        beta_company = company.name
+   end
+end
+    puts "I found #{beta_company}!"
+
+largest_company = companies.max {|company| company.size}  
+puts largest_company
+
+sorted_companies = companies.reverse 
+puts sorted_companies
+
